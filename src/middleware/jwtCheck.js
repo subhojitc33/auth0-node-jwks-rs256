20df7ed1f72a39aa55cb033468e03a9b69766720
subhojitc33 <subhojitc3@gmail.com>
2019-04-12 18:55:08 +0530

    Update jwtCheck.js

--- src/middleware/jwtCheck.js
+++ src/middleware/jwtCheck.js
@@ -7,12 +7,12 @@ dotenv.load();
 
 export const jwtCheck = expressJwt({
   secret: expressJwtSecret({
-    jwksUri: `https://demo.identityserver.io/.well-known/openid-configuration/jwks`
+    jwksUri: `https://www.googleapis.com/oauth2/v3/certs`
   }),
 
   // Validate the audience and the issuer.
-  audience: 'https://demo.identityserver.io/resources',
-  issuer: `https://demo.identityserver.io`,
+  audience: '636630247382-bv80b12tgd0nrsjkv2urljpauiq6thsi.apps.googleusercontent.com',
+  issuer: `https://accounts.google.com`,
   algorithms: ['RS256']
 });
 
